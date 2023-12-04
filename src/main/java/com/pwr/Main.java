7de318fec79eb9c2a4083820a3a551b6dcddadc1
makszdanowicz <273564@student.pwr.edu.pl>
2023-12-04 09:10:06 +0100

    version with db

--- src/main/java/com/pwr/Main.java
+++ src/main/java/com/pwr/Main.java
@@ -2,7 +2,7 @@ package com.pwr;
 
 import com.pwr.gui.InformationCollector;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
         InformationCollector collector = new InformationCollector();
         collector.getDecision();
