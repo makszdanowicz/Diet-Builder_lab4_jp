127e1ba86133735358d57605d01f8e845b1d9c59
makszdanowicz <273564@student.pwr.edu.pl>
2023-12-04 11:05:38 +0100

    added FXML.loader

--- src/main/java/com/pwr/Main.java
+++ src/main/java/com/pwr/Main.java
@@ -2,9 +2,13 @@ package com.pwr;
 
 import com.pwr.gui.InformationCollector;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -15,8 +19,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(new StackPane(),640,480);
-        stage.setTitle("Diet-Builder");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainView.fxml"));
+        Parent root = loader.load();
+        //Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+        Scene scene = new Scene(root,640,480);
+        stage.setTitle("DietBuilder");
         stage.setScene(scene);
         stage.show();
     }
