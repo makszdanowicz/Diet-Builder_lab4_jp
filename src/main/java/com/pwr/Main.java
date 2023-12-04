546088afc1e19b81cf052b3552977162ba430445
makszdanowicz <273564@student.pwr.edu.pl>
2023-12-04 10:16:33 +0100

    connected javafx library to a project

--- src/main/java/com/pwr/Main.java
+++ src/main/java/com/pwr/Main.java
@@ -1,10 +1,23 @@
 package com.pwr;
 
 import com.pwr.gui.InformationCollector;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main{
+public class Main extends Application {
     public static void main(String[] args) {
         InformationCollector collector = new InformationCollector();
-        collector.getDecision();
+        //collector.getDecision();
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(new StackPane(),640,480);
+        stage.setTitle("Diet-Builder");
+        stage.setScene(scene);
+        stage.show();
     }
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module Lab4 {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+    opens com.pwr;
+}
