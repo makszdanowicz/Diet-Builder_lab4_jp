82c5f512ff6b8db905dc705446b0c71c6bfae00d
makszdanowicz <273564@student.pwr.edu.pl>
2023-12-05 22:57:45 +0100

    added switching between scenes

---
+++ src/main/java/com/pwr/gui/InformationController.java
@@ -0,0 +1,24 @@
+package com.pwr.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class InformationController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button menuButton;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
--- src/main/java/com/pwr/gui/MainViewController.java
+++ src/main/java/com/pwr/gui/MainViewController.java
@@ -1,12 +1,17 @@
 package com.pwr.gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class MainViewController {
 
@@ -24,10 +29,22 @@ public class MainViewController {
         launchAppButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                System.out.println("You just click on me!");
+                launchAppButton.getScene().getWindow().hide();
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/InstructionView.fxml"));
+                try {
+                    Parent root = loader.load();
+                    Stage stage = new Stage();
+                    stage.setScene(new Scene(root));
+                    stage.setTitle("DietBuilder-Instruction");
+                    stage.setResizable(false);
+                    stage.showAndWait();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
             }
         });
 
     }
-
 }
